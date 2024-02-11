7bf05980d64daa7f8ae55a23d639515887cd0437
Dmitriy <barashenkov@inbox.ru>
2024-02-11 15:58:36 +0300

    Comments added. Added type annotations.

--- menu_functions/display_entries.py
+++ menu_functions/display_entries.py
@@ -1,27 +1,30 @@
-# Телефонная книга
+# Телефонная книга, функция вызова всех записей и поиска по записям
 
 
 def display_entries():
     with open("phonebook.txt", "r") as file: # открываем файл для чтения
         lines = file.readlines()
+        # принимает на вход номер страницы (page_number) и размер страницы (page_size)
         page_size: int = 10  # количество строк на странице
         page_number: int = 1
-
+        # позволяет показывать информацию из списка lines постранично
 
         while True:
+            # вычисляет индексы начального (start_index) и конечного (end_index) элементов на данной странице
             start_index = (page_number - 1) * page_size
             end_index = page_number * page_size
 
             for i in range(start_index, min(end_index, len(lines))):
                 print(lines[i])
-
+            # выводит элементы списка с индексами от start_index до end_index или до конца списка (len(lines)) при условии,
+            # что end_index меньше или равен длине списка.
             if end_index >= len(lines):
                 break
 
             user_input = input("Нажмите Enter для перехода на следующую страницу или «q», чтобы выйти: ")
             if user_input == "q":
                 break
-
+            # номер страницы увеличивается на 1 (page_number += 1), и процесс повторяется для следующей страницы
             page_number += 1
 
     # display_entries(): выводит все записи из файла "phonebook.txt" на экран,
@@ -30,12 +33,14 @@ def display_entries():
 
 
 
-def search_entries(last_name="", first_name="", organization=""):
+def search_entries(last_name: str="", first_name: str="", organization: str=""):
+    # считывает содержимое файла "phonebook.txt"
     with open("phonebook.txt", "r") as file:
-        lines = file.readlines()
-
+        # считывает все строки из файла и возвращает список из всех считанных строк
+        lines: list[str] = file.readlines()
+        # проверяет каждую запись на соответствие заданным параметрам поиска (фамилии, имени и организации)
         for line in lines:
-            entry = line.split(",")
+            entry: list[str] = line.split(",")
             if last_name and entry[0] != last_name:
                 continue
 
--- menu_functions/editing_in_catalog.py
+++ menu_functions/editing_in_catalog.py
@@ -1,4 +1,9 @@
+# функции добавления новых записей и редактирование старых
+
+# принимает входные параметры: ФИО, организация, телефонные номера
 def add_entry(last_name: str, first_name: str, middle_name: str, organization: str, work_phone: str, personal_phone: str):
+    # открывает файл "phonebook.txt" в режиме дозаписи (если файл не существует, он будет создан)
+    # и записывает новую запись в файл в формате CSV, разделяя каждое поле запятой
     with open("phonebook.txt", "a") as file:
         file.write(f"{last_name},{first_name},{middle_name},{organization},{work_phone},{personal_phone}\n")
 
@@ -7,14 +12,18 @@ def add_entry(last_name: str, first_name: str, middle_name: str, organization: s
     # add_entry(): добавляет новую запись в файл "phonebook.txt".
     # Пользователь должен указать фамилию, имя, отчество, организацию, рабочий и мобильный телефон
 
-
+# заменяет существующую запись с указанной фамилией на новые значения
 def edit_entry(last_name: str, new_last_name: str, new_first_name: str, new_middle_name: str, new_organization: str, new_work_phone: str, new_personal_phone: str):
+    # открывает файл "phonebook.txt", считывает все строки,
     with open("phonebook.txt", "r+") as file:
-        lines = file.readlines()
-
+        lines: list[str] = file.readlines()
+        # ищет запись с указанной фамилией
         for i in range(len(lines)):
-            entry = lines[i].split(",")
+            # проходим по списку
+            entry = lines[i].split(",") # разбивает строку lines[i] на подстроки, разделенные запятыми, и сохраняет их в список entry
+            # В списке entry по индексу 0 ищем соответствие
             if entry[0] == last_name:
+                # заменяет значения полей на новые значения и перезаписывает измененные строки обратно в файл.
                 entry[0] = new_last_name
                 entry[1] = new_first_name
                 entry[2] = new_middle_name
@@ -24,9 +33,10 @@ def edit_entry(last_name: str, new_last_name: str, new_first_name: str, new_midd
 
                 lines[i] = ",".join(entry)
 
-        file.seek(0)
+        file.seek(0) # Устанавливает позицию чтения/записи файла в начало файла.
         file.truncate()
-        file.writelines(lines)
+        file.writelines(lines) # Записывает строки, указанные в переменной lines, в файл.
+                               # Каждая строка будет записана на отдельной строке в файле.
 
     print("Запись успешно обновлена!")
 
